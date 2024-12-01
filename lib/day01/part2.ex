cc1725c731fc66b387aae3f52a9a21f0727ec066
Val Aleksenko <v.aleksenko@gmail.com>
2024-12-01 10:09:00 -0500

    d1p2

---
+++ lib/day01/part2.ex
@@ -0,0 +1,28 @@
+defmodule AoC2024.Day01.Part2 do
+  @moduledoc """
+    @see https://adventofcode.com/2024/day/1#part2
+  """
+  @behaviour AoC2024.Day
+
+  @impl AoC2024.Day
+
+  def run(data) do
+    data
+    |> Enum.map(&to_tuples/1)
+    |> Enum.unzip()
+    |> to_scores()
+    |> Enum.sum()
+  end
+
+  defp to_tuples(input) do
+    input
+    |> String.split
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  defp to_scores({l1, l2}) do
+    l1
+    |> Enum.map(fn e -> e * Enum.count(l2, &(&1 == e)) end)
+  end
+end
---
+++ test/day01/part2_test.exs
@@ -0,0 +1,10 @@
+defmodule AoC2024.Day01.Part2Test do
+  use ExUnit.Case
+  doctest AoC2024.Day01.Part2
+  import AoC2024.Day01.Part2
+  import TestHelper
+
+  test "runs for sample input" do
+    assert 31 == run(read_example(:day01))
+  end
+end
