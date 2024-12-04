8624ae7c9731b95c228f1ffd20d2d2e291fef5bc
Val Aleksenko <v.aleksenko@gmail.com>
2024-12-04 11:16:23 -0500

    d4p1

---
+++ lib/day04/part1.ex
@@ -0,0 +1,48 @@
+defmodule AoC2024.Day04.Part1 do
+  @moduledoc """
+    @see https://adventofcode.com/2024/day/4
+  """
+  @behaviour AoC2024.Day
+
+  @impl AoC2024.Day
+
+  @word ["X", "M", "A", "S"]
+  @neighbors for x <- -1..1, y <- -1..1, {x, y} != {0, 0}, do: {x, y}
+
+  def run(data) do
+    data
+    |> to_grid()
+    |> word_search()
+    |> length()
+  end
+
+  defp word_search(grid) do
+    grid
+    |> Enum.flat_map(&(run_search(&1, grid)))
+    |> Enum.reject(&Enum.empty?/1)
+    |> Enum.uniq()
+  end
+
+  defp run_search(start, grid) do
+    @neighbors
+    |> Enum.flat_map(&(search({[], @word, []}, start, &1, grid))) 
+  end
+
+  defp search({found, [], current}, _, _, _), do: [current | found]
+  defp search({found, [letter | reminder], current}, {{x, y}, letter}, {xd, yd}, grid), do:
+    search({found, reminder, [{x, y} | current]}, {{x + xd, y + yd}, grid[{x + xd, y + yd}]}, {xd, yd}, grid)
+  defp search({found, _, _}, _, _, _), do: found
+
+  defp to_grid(data) do
+    data
+    |> Enum.with_index()
+    |> Enum.reduce(Map.new(), &add_row/2)
+  end
+
+  defp add_row({row, y}, grid) do
+    row
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.reduce(grid, fn {v, x}, m -> Map.put(m, {x, y}, v) end)
+  end
+end
---
+++ test/day04/part1_test.exs
@@ -0,0 +1,10 @@
+defmodule AoC2024.Day04.Part1Test do
+  use ExUnit.Case
+  doctest AoC2024.Day04.Part1
+  import AoC2024.Day04.Part1
+  import TestHelper
+
+  test "runs for sample input" do
+    assert 18 == run(read_example(:day04))
+  end
+end
